02839b6efa7f8e1c5b51a59d6d01a7581b1a7857
Silvia Patricia Soto <122170353+PatriciaB612@users.noreply.github.com>
2023-05-15 15:23:55 -0400

    fixing js functionality

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const billInput = document.querySelector('.bill-input')
 const tipBtns = document.querySelectorAll('.tip-btn')
+const inputs = document.querySelectorAll('.input')
 const peopleNumber = document.querySelector('.people-number-input')
 const tipPerPersonEl = document.querySelector('.tip-per-person')
 const totalPerPersonEl = document.querySelector('.total-per-person')
 const resetBtn = document.querySelector('.reset-btn')
+const errorMessage = document.querySelector('.error-message')
 
 //Give pressed button an active class
 tipBtns.forEach(function (tipBtn) {
   tipBtn.addEventListener('click', function () {
     document.querySelector('.active')?.classList.remove('active')
     tipBtn.classList.add('active')
+    if (peopleNumber.value > 0) {
+      calculateTip()
+    }
   })
 })
 
@@ -33,16 +38,29 @@ function renderAmounts(tip, total) {
   totalPerPersonEl.innerText = `$${total.toFixed(2)}`
 }
 
-peopleNumber.addEventListener('keypress', function (e) {
-  if (e.key === 'Enter') {
-    calculateTip()
-  }
-})
-
 resetBtn.addEventListener('click', function () {
-  billInput.value = 0
+  billInput.value = ''
   document.querySelector('.active').classList.remove('active')
-  peopleNumber.value = 0
+  customTipInput.value = ''
+  peopleNumber.value = ''
+  peopleNumber.style.outline = 'none'
+  errorMessage.style.display = 'none'
   tipPerPersonEl.innerHTML = '$0.00'
   totalPerPersonEl.innerHTML = '$0.00'
+})
+
+//run calculateTip() when 'enter' is pressed in any input
+
+inputs.forEach(function (input) {
+  input.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter' && peopleNumber.value != 0) {
+      calculateTip()
+      peopleNumber.style.outline = '3px solid #26c2ae'
+      errorMessage.style.display = 'none'
+    }
+    if (e.key === 'Enter' && peopleNumber.value == 0) {
+      peopleNumber.style.outline = '3px solid #E17457'
+      errorMessage.style.display = 'inline'
+    }
+  })
 })
